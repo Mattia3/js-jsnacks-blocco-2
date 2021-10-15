a8330d0f4d15f2b5f15eb54cab6aeda019eed1b3
Mattia3 <mcortinovis9@gmail.com>
2021-10-15 16:48:12 +0200

    Esercizio numero casuale

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Stampa un numero casuale
+const numeroRandom = Math.ceil(Math.random()* 100);
+console.log(numeroRandom);
+//se è pari
+if(numeroRandom%2 === 0){
+  console.log("pari")
+  //Stampa questo codice
+  //se è dispari
+}else{
+  console.log("dispari")
+  console.log(casualNumber +1)
+  //stampa questo codice
+}
